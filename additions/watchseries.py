84f2586a48d890f533c85beda62a17ee997b4bb8
einfall <nix>
2013-06-04 15:37:15 +0200

    add Newest / Popular episodes + TV Schedule

--- additions/watchseries.py
+++ additions/watchseries.py
@@ -57,10 +57,10 @@ class watchseriesGenreScreen(Screen):
 		self.onLayoutFinish.append(self.loadPage)
 		
 	def loadPage(self):	
-		self.genreliste = [('Series',"http://watchseries.lt/letters/")]
-							#('Newest Episodes Added',"http://watchseries.lt/latest"),
-							#('Popular Episodes Added This Week',"http://watchseries.lt/new"),
-							#('TV Schedule',"http://watchseries.lt/tvschedule/-1")]
+		self.genreliste = [('Series',"http://watchseries.lt/letters/"),
+							('Newest Episodes Added',"http://watchseries.lt/latest"),
+							('Popular Episodes Added This Week',"http://watchseries.lt/new"),
+							('TV Schedule',"http://watchseries.lt/tvschedule/-1")]
 							
 		self.chooseMenuList.setList(map(watchseriesGenreListEntry, self.genreliste))
 		self.keyLocked = False
@@ -72,12 +72,83 @@ class watchseriesGenreScreen(Screen):
 		
 		if streamGenreName == "Series":
 			self.session.open(watchseriesSeriesLetterScreen, streamGenreLink, streamGenreName)
-		elif streamGenreName == "Newest Episodes Added":
-			self.session.open(watchseriesSeriesScreen, streamGenreLink, streamGenreName)
-		elif streamGenreName == "Popular Episodes Added This Week":
-			self.session.open(watchseriesSeriesScreen, streamGenreLink, streamGenreName)
-		elif streamGenreName == "TV Schedule":
-			self.session.open(watchseriesSeriesScreen, streamGenreLink, streamGenreName)
+		#elif streamGenreName == "Newest Episodes Added":
+		else:
+			self.session.open(watchseriesNewSeriesScreen, streamGenreLink, streamGenreName)
+		#elif streamGenreName == "Popular Episodes Added This Week":
+		#	self.session.open(watchseriesSeriesScreen, streamGenreLink, streamGenreName)
+		#elif streamGenreName == "TV Schedule":
+		#	self.session.open(watchseriesSeriesScreen, streamGenreLink, streamGenreName)
+
+	def keyCancel(self):
+		self.close()
+
+class watchseriesNewSeriesScreen(Screen):
+	
+	def __init__(self, session, streamGenreLink, streamGenreName):
+		self.session = session
+		self.streamGenreLink = streamGenreLink
+		self.streamGenreName = streamGenreName
+		self.plugin_path = mp_globals.pluginPath
+		self.skin_path =  mp_globals.pluginPath + "/skins"
+		
+		path = "%s/%s/defaultGenreScreen.xml" % (self.skin_path, config.mediaportal.skin.value)
+		if not fileExists(path):
+			path = self.skin_path + "/original/defaultGenreScreen.xml"
+
+		with open(path, "r") as f:
+			self.skin = f.read()
+			f.close()
+			
+		Screen.__init__(self, session)
+		
+		self["actions"]  = ActionMap(["OkCancelActions", "ShortcutActions", "WizardActions", "ColorActions", "SetupActions", "NumberActions", "MenuActions"], {
+			"ok"    : self.keyOK,
+			"cancel": self.keyCancel
+		}, -1)
+		
+		self.keyLocked = True
+		self['title'] = Label("watchseries.lt")
+		self['ContentTitle'] = Label("%s:" % self.streamGenreName)
+		self['name'] = Label("")
+		self['F1'] = Label("Exit")
+		self['F2'] = Label("")
+		self['F3'] = Label("")
+		self['F4'] = Label("")
+		self['F3'].hide()
+		self['F4'].hide()
+		
+		self.genreliste = []
+		self.chooseMenuList = MenuList([], enableWrapAround=True, content=eListboxPythonMultiContent)
+		self.chooseMenuList.l.setFont(0, gFont('mediaportal', 23))
+		self.chooseMenuList.l.setItemHeight(25)
+		self['genreList'] = self.chooseMenuList
+		
+		self.onLayoutFinish.append(self.loadPage)
+		
+	def loadPage(self):
+		print self.streamGenreLink
+		getPage(self.streamGenreLink, headers={'Content-Type':'application/x-www-form-urlencoded'}).addCallback(self.loadPageData).addErrback(self.dataError)
+		
+	def dataError(self, error):
+		print error
+		
+	def loadPageData(self, data):
+		eps = re.findall('<li><a title=".*?" href="(/episode/.*?)">(.*?)</a></li>', data, re.S)
+		if eps:
+			for link,title in eps:
+				title = title.replace('Seas. ','- S').replace('Ep. ','E')
+				url = "http://watchseries.lt%s" % link
+				self.genreliste.append((title, url))				
+			self.chooseMenuList.setList(map(watchseriesFilmListEntry, self.genreliste))
+			self.keyLocked = False
+
+	def keyOK(self):
+		streamGenreName = self['genreList'].getCurrent()[0][0]
+		streamGenreLink = self['genreList'].getCurrent()[0][1]
+		print streamGenreName, streamGenreLink
+		
+		self.session.open(watchseriesStreamListeScreen, streamGenreLink, streamGenreName)
 
 	def keyCancel(self):
 		self.close()
@@ -424,22 +495,31 @@ class watchseriesStreamListeScreen(Screen):
 		print error
 		
 	def loadPageData(self, data):
-		print "daten bekommen"
-		streams = re.findall('</td></tr><tr.*?><td><span>(.*?)</span></td><td> <a target="_blank" href="(/open/cale/.*?)"', data, re.S)
-		if streams:
+		print "daten bekommen: streamlist"
+		if re.match('.*?there are no links available for this episode', data, re.S|re.I):
 			self.filmliste = []
-			for (shname,shurl) in streams:
-				print shname,shurl
-				if re.match('.*?(sharesix|putme|limevideo|stream2k|played|putlocker|sockshare|streamclou|xvidstage|filenuke|movreel|nowvideo|xvidstream|uploadc|vreer|MonsterUploads|Novamov|Videoweed|Divxstage|Ginbig|Flashstrea|Movshare|yesload|faststream|Vidstream|PrimeShare|flashx|Divxmov|Zooupload|Wupfile|BitShare|Userporn)', shname, re.S|re.I):
-					url = "http://watchseries.lt%s" % shurl
-					self.filmliste.append((decodeHtml(shname),url))
-					
-			if len(self.filmliste) == 0:
-				self.filmliste.append(("No supported streams found.", "No supported streams found."))
-				self.chooseMenuList.setList(map(watchseriesHosterListEntry, self.filmliste))
+			self.filmliste.append(("There are no links available for this episode", "No supported streams found."))
+			self.chooseMenuList.setList(map(watchseriesHosterListEntry, self.filmliste))
+		else:
+			streams = re.findall('</td></tr><tr.*?><td><span>(.*?)</span></td><td> <a target="_blank" href="(/open/cale/.*?)"', data, re.S)
+			print streams
+			if streams:
+				self.filmliste = []
+				for (shname,shurl) in streams:
+					print shname,shurl
+					if re.match('.*?(sharesix|putme|limevideo|stream2k|played|putlocker|sockshare|streamclou|xvidstage|filenuke|movreel|nowvideo|xvidstream|uploadc|vreer|MonsterUploads|Novamov|Videoweed|Divxstage|Ginbig|Flashstrea|Movshare|yesload|faststream|Vidstream|PrimeShare|flashx|Divxmov|Zooupload|Wupfile|BitShare|Userporn)', shname, re.S|re.I):
+						url = "http://watchseries.lt%s" % shurl
+						self.filmliste.append((decodeHtml(shname),url))
+						
+				if len(self.filmliste) == 0:
+					self.filmliste.append(("No supported streams found.", "No supported streams found."))
+					self.chooseMenuList.setList(map(watchseriesHosterListEntry, self.filmliste))
+				else:
+					self.chooseMenuList.setList(map(watchseriesHosterListEntry, self.filmliste))
+					self.keyLocked = False
 			else:
+				self.filmliste.append(("Wrong parsing..", "No supported streams found."))
 				self.chooseMenuList.setList(map(watchseriesHosterListEntry, self.filmliste))
-				self.keyLocked = False
 					
 	def keyOK(self):
 		if self.keyLocked:
