18545611a02b58892dc5e179a5f8fb33be7a2107
Billy2011 <kschmidt2007@googlemail.com>
2013-06-06 15:52:15 +0200

    youtubeplayer: youtubelink class added

---
+++ resources/youtubelink.py
@@ -0,0 +1,21 @@
+#	-*-	coding:	utf-8	-*-
+
+from Components.config import config
+from Plugins.Extensions.MediaPortal.resources.yt_url import *
+
+class YoutubeLink:
+	def __init__(self, session):
+		print "YoutubeLink:"
+		self.session = session
+		self._callback = None
+		self.title = ''
+		self.videoPrio = int(config.mediaportal.youtubeprio.value)-1
+		
+	def getLink(self, cb_play, cb_err, title, url):
+		self._callback = cb_play
+		self.title = title
+		y = youtubeUrl(self.session)
+		y.addErrback(cb_err)
+		link = y.getVideoUrl(url, self.videoPrio)
+		self._callback(title, link)
+
--- resources/youtubeplayer.py
+++ resources/youtubeplayer.py
@@ -1,26 +1,18 @@
 #	-*-	coding:	utf-8	-*-
 
-from Components.config import config
-from Plugins.Extensions.MediaPortal.resources.yt_url import *
 from Plugins.Extensions.MediaPortal.resources.simpleplayer import SimplePlayer
+from Plugins.Extensions.MediaPortal.resources.youtubelink import YoutubeLink
 
 class YoutubePlayer(SimplePlayer):
 
 	def __init__(self, session, playList, playIdx=0, playAll=False, listTitle=None, plType='local', title_inr=0):
 		print "YoutubePlayer:"
-		self.videoPrio = int(config.mediaportal.youtubeprio.value)-1
 		
-		SimplePlayer.__init__(self, session, playList, playIdx, playAll, listTitle, plType, title_inr)
+		SimplePlayer.__init__(self, session, playList, playIdx, playAll, listTitle, plType, title_inr, False, 'youtube')
 		
 	def getVideo(self):
 		print "getVideo:"
-		self.videoPrio = int(config.mediaportal.youtubeprio.value)-1
 		dhTitle = self.playList[self.playIdx][self.title_inr]
 		dhVideoId = self.playList[self.playIdx][2]
-		print "Title: ",dhTitle
-		#print "VideoId: ",dhVideoId
-		y = youtubeUrl(self.session)
-		y.addErrback(self.dataError)
-		dhLink = y.getVideoUrl(dhVideoId, self.videoPrio)
-		if dhLink:
-			self.playStream(dhTitle, dhLink)
+		YoutubeLink(self.session).getLink(self.playStream, self.dataError, dhTitle, dhVideoId)
+		
